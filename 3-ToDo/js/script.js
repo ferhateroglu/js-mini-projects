21805a0b0bb7ebc86bf37b56c00bef63382fdbc5
Ferhat Eroglu <ferhateroglu1907@gmail.com>
2022-01-19 22:42:20 +0300

    added functions for delete and check todo

--- 3-ToDo/js/script.js
+++ 3-ToDo/js/script.js
@@ -1,44 +1,51 @@
-const ulDOM = document.querySelector('#list');
-const liveToastAdded = document.querySelector('#liveToastAdded');
-const liveToastEmpty = document.querySelector('#liveToastEmpty');
+const ulDOM = document.querySelector("#list");
+const liveToastAdded = document.querySelector("#liveToastAdded");
+const liveToastEmpty = document.querySelector("#liveToastEmpty");
 
-//Select li Elment
-ulDOM.addEventListener('click', (item) => {
-    if(item.target.tagName == 'LI'){
-        
-    }
-});
+ulDOM.addEventListener("click", (item) => checkOrDelete(item));
 
 
+// check or delete li 
+const checkOrDelete = (item) => {
+  // li checked or remove checked
+  if (item.target.tagName == "LI") {
+    //item.target.classList.add("checked");
+    const isChecked = item.target.classList.contains('checked');
+    isChecked == (true) ? item.target.classList.remove('checked') : item.target.classList.add('checked');
+  }
+  // li remove
+  else {
+    ulDOM.removeChild(item.target.parentNode);
+  }
+};
 
 //Create New Element
-function newElement(){
-    const task = document.querySelector('#task');
+const newElement = () => {
+  const task = document.querySelector("#task");
 
-    if(task.value.trim() == ''){
-        liveTaskShow(liveToastEmpty);
-    }else{
-        console.log('deneme');
-        liveTaskShow(liveToastAdded);
-        addLi(task.value);
-    }
-}
+  if (task.value.trim() == "") {
+    liveTaskShow(liveToastEmpty);
+  } else {
+    console.log("deneme");
+    liveTaskShow(liveToastAdded);
+    addLi(task.value);
+  }
+};
 
-const addLi = (task) =>{
-    let newLi = document.createElement('li');
-    newLi.innerHTML = `${task}<span class="close">x</span>`;
-    ulDOM.append(newLi);
-}
+const addLi = (task) => {
+  let newLi = document.createElement("li");
+  newLi.innerHTML = `${task}<span class="close">x</span>`;
+  ulDOM.append(newLi);
+};
 
-const liveTaskShow = (liveToastDOM) =>{
-    liveToastDOM.classList.remove('hide');
-    liveToastDOM.classList.add('show');
-}
+const liveTaskShow = (liveToastDOM) => {
+  liveToastDOM.classList.remove("hide");
+  liveToastDOM.classList.add("show");
+};
 
-const liveTaskHide = () =>{
-    liveToastAdded.classList.remove('show');
-    liveToastAdded.classList.add('hide');
-    liveToastEmpty.classList.remove('show');
-    liveToastEmpty.classList.add('hide');
-}
-
+const liveTaskHide = () => {
+  liveToastAdded.classList.remove("show");
+  liveToastAdded.classList.add("hide");
+  liveToastEmpty.classList.remove("show");
+  liveToastEmpty.classList.add("hide");
+};
